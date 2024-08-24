37e55282c3786b05db71ca54200070db850a084b
Connor Link <connorjlink05@gmail.com>
2024-08-24 02:36:38 -0700

    Improve null-opt errors

--- Error.cpp
+++ Error.cpp
@@ -6,7 +6,13 @@ namespace hz
 {
 	std::string Error::format() const
 	{
-		return std::format("{} at {}:{}\n  from {}:\n  {}",
-			_severity_map.at(_severity), _position._line, _position._column, _erroring_line, _message);
+		if (_position._line != 0 && _position._column != 0 && _erroring_line != "")
+		{
+			return std::format("{} at {}:{}\n  from {}:\n    {}",
+				_severity_map.at(_severity), _position._line, _position._column, _erroring_line, _message);
+		}
+
+		return std::format("{}\n    {}",
+			_severity_map.at(_severity), _message);
 	}
 }
