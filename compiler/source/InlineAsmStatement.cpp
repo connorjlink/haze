b3caf758ae020f4eca4857bb3ad1d3b497609dfe
Connor Link <connor@connorjlink.com>
2026-03-21 01:31:35 -0500

    Byterange tweak

--- compiler/source/InlineAsmStatement.cpp
+++ compiler/source/InlineAsmStatement.cpp
@@ -29,7 +29,7 @@ namespace hz
 		const auto object_code = commands
 			| std::ranges::views::transform([&](auto command) { return command->object_code; })
 			| std::ranges::views::join
-			| std::ranges::to<std::vector<std::uint8_t>>();
+			| std::ranges::to<byterange>();
 
 		REQUIRE_SAFE(Generator)->inline_assembly(std::move(object_code), object_code.size());
 	}
